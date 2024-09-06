12a663d2dfdb0246a55a445318e06fd7c52fa4b3
tkdhimanshusingh <tkdhimanshusingh@gmail.com>
2024-09-06 11:45:48 +0530

    Time: 35 ms (72.76%) | Memory: 16.5 MB (90.25%) - LeetSync

---
+++ 71-simplify-path/simplify-path.py
@@ -0,0 +1,13 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        stack = []
+        directories = path.split('/')
+        for dir in directories:
+            if dir == '.' or not dir:
+                continue
+            elif dir == '..':
+                if stack:
+                    stack.pop()
+            else:
+                stack.append(dir)
+        return '/' + '/'.join(stack)
